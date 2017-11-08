b5d1259f9619e648a358632b3cdb36a4d3b359cf
Raphael Varonos <r.varonos@gmail.com>
2017-11-08 11:58:33 +0200

    minor fix

--- commands/image/start.js
+++ commands/image/start.js
@@ -5,11 +5,11 @@ module.exports = function() {
   proc.exec('docker-compose up -d --force-recreate dev')
     .then(function() {
 
-      var terminal = 'x-terminal-emulator -e "docker-compose exec dev bash -c';
-
-      proc.exec(`${terminal} 'tail -n 1000 -f .pm2/logs/output-0.log'"`);
-      proc.exec(`${terminal} 'tail -n 1000 -f .pm2/logs/error-0.log'"`);
-      proc.exec(`${terminal} 'agneta process list; exec /bin/bash -i'"`);
+      var terminal = 'x-terminal-emulator -e "docker-compose exec dev bash -c  \'';
+      var ending = '\'; bash"';
+      proc.exec(`${terminal}tail -n 1000 -f .pm2/logs/output-0.log${ending}`);
+      proc.exec(`${terminal}tail -n 1000 -f .pm2/logs/error-0.log${ending}`);
+      proc.exec(`${terminal}agneta process list; exec /bin/bash -i${ending}`);
     });
 
 };
