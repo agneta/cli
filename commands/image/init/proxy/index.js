82328bebc977b7a5784747dfdff524397100665d
Raphael Varonos <r.varonos@gmail.com>
2017-11-22 12:50:21 +0200

    Setup live mode for local proxy server

--- commands/image/init/proxy/index.js
+++ commands/image/init/proxy/index.js
@@ -1,10 +1,20 @@
 const fs = require('fs-extra');
 const path = require('path');
+const config = require('../../config');
+const _ = require('lodash');
 module.exports = function() {
 
   var pathProxy = path.join(process.cwd(), '.proxy');
 
-  return fs.copy(path.join(__dirname, 'config.cfg'), path.join(pathProxy, 'config.cfg'))
+  return fs.readFile(path.join(__dirname, 'config.cfg'))
+    .then(function(content) {
+
+      content = _.template(content)({
+        config: config
+      });
+
+      return fs.outputFile(path.join(pathProxy, 'config.cfg'),content);
+    })
     .then(function() {
       return fs.copy(path.join(__dirname, 'dockerfile'), path.join(pathProxy, 'dockerfile'));
     });
