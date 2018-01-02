8c6750287d0eb5dac13d380ce90b8cbb69a174e3
Raphael Varonos <r.varonos@agneta.io>
2018-01-02 14:22:01 +0200

    Delete process from pm2 for a better stop and restart

--- commands/process/stop.js
+++ commands/process/stop.js
@@ -2,12 +2,33 @@ const pm2 = require('pm2');
 const Promise = require('bluebird');
 
 function promise() {
+  var name = 'agneta';
 
   return Promise.resolve()
     .then(function() {
       return new Promise(function(resolve,reject) {
 
-        pm2.stop('agneta',function(err,list){
+        pm2.describe(name,function(err,result){
+
+          if(err){
+            reject(err);
+          }
+
+          resolve(result);
+        });
+
+      });
+    })
+    .then(function(result) {
+
+      if(!result[0]){
+        console.log('Process is not running');
+        return;
+      }
+
+      return new Promise(function(resolve,reject) {
+
+        pm2.delete(name,function(err,list){
 
           if(err){
             reject(err);
@@ -16,7 +37,10 @@ function promise() {
           resolve(list);
         });
 
-      });
+      })
+        .then(function() {
+          console.log('process stopped');
+        });
     });
 
 }
@@ -25,9 +49,9 @@ module.exports = {
   promise: promise,
   command: function(){
     promise()
-      .then(function(list) {
-        console.log(list);
+      .then(function() {
         process.exit();
-      });
+      })
+      .catch(console.error);
   }
 };
