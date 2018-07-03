6d7e03b5d728e6c5a86364825c902593a4bcb293
Raphael Varonos <r.varonos@agneta.io>
2018-07-03 11:45:25 +0200

    Start with ts-node if typescript is available

--- commands/start.js
+++ commands/start.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const pm2 = require('pm2');
+const fs = require('fs-extra');
 
 function promise() {
-
   // Default is portal
   process.env.NODE_ENV = process.env.NODE_ENV || 'development';
   process.env.MODE = process.env.MODE || 'portal';
@@ -14,7 +14,6 @@ function promise() {
       return require('./secret/key/get').promise();
     })
     .then(function() {
-
       return new Promise(function(resolve, reject) {
         pm2.connect(function(err) {
           if (err) {
@@ -31,51 +30,65 @@ function promise() {
         var outputPath = path.join(base, `${name}-output.log`);
         var errorPath = path.join(base, `${name}-error.log`);
 
-        pm2.start({
+        var scriptArgs;
+        var scriptInterpreter;
+        var scriptDir = path.join(global.pathPlatform, 'main', 'server');
+        var scriptPath = path.join(scriptDir, 'index.ts');
+
+        if (fs.existsSync(scriptPath)) {
+          scriptArgs = ['--transpileOnly'];
+          scriptInterpreter = path.join(
+            __dirname,
+            '../node_modules/ts-node/dist/bin.js'
+          );
+        } else {
+          scriptPath = path.join(scriptDir, 'index.js');
+        }
+
+        var pm2Options = {
           name: name,
-          script: path.join(global.pathPlatform,'main', 'server', 'index.js'),
+          script: scriptPath,
+          args: scriptArgs,
+          interpreter: scriptInterpreter,
+          sourceMapSupport: true,
+          min_uptime: '1m',
+          max_restarts: 10,
           exec_mode: 'fork',
           logDateFormat: '>> YYYY-MM-DD HH:mm:ss Z :',
           output: outputPath,
           error: errorPath,
           cwd: process.cwd()
-        }, function(err) {
+        };
+
+        pm2.start(pm2Options, function(err) {
           if (err) {
             return reject(err);
           }
           resolve();
         });
       });
-
     });
-
 }
 
 module.exports = {
   promise: promise,
-  command: function(yargs){
-
+  command: function(yargs) {
     var argv = yargs
       .alias('m', 'mode')
       .describe('m', 'Select build mode')
       .choices('m', ['sftp', 'services'])
-      .help('help')
-      .argv;
+      .help('help').argv;
 
     switch (argv.m) {
-
       case 'sftp':
-
         process.env.MODE = 'sftp';
         process.env.PORT = process.env.PORT || 8282;
         break;
 
       case 'services':
-
         process.env.MODE = 'services';
         process.env.PORT = process.env.PORT || 8484;
         break;
-
     }
 
     promise()
@@ -87,6 +100,5 @@ module.exports = {
         //process.exit();
       })
       .catch(console.error);
-
   }
 };
