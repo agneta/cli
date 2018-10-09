681eca3e24d0503d9026be2d057fb048bd0a2522
Raphael Varonos <r.varonos@agneta.io>
2018-10-09 09:33:55 +0200

    Generate dependencies

--- src/commands/generate/index.js
+++ src/commands/generate/index.js
@@ -1,13 +1,21 @@
+const path = require('path');
+
 module.exports = function(yargs) {
-
   yargs.command('generate', 'Generation tool', function(yargs) {
-
-    yargs.command('dependencies', 'Load from bower compomnents', init('dependencies'));
-    yargs.command('scripts', 'Create bundled scripts for your project', init('scripts'));
+    yargs.command(
+      'dependencies',
+      'Load from bower compomnents',
+      init('dependencies')
+    );
+    yargs.command(
+      'scripts',
+      'Create bundled scripts for your project',
+      init('scripts')
+    );
 
     function init(name) {
-
       const terminal = global.requireMain('server/terminal');
+      const progress = global.requireMain('progress');
 
       return function() {
         Promise.resolve()
@@ -19,9 +27,18 @@ module.exports = function(yargs) {
             return terminal();
           })
           .then(function(servers) {
-            return require(`../image/setup/${name}`)(servers);
+            console.log(servers.webPages.locals.project.paths);
+            var paths = servers.webPages.locals.project.paths;
+            return require(path.join(
+              paths.portal.website,
+              `utilities/generate/${name}`
+            ))({
+              locals: servers.webPortal.locals,
+              log: console.log,
+              progress: progress
+            });
           })
-          .catch(function(err){
+          .catch(function(err) {
             console.error(err);
           })
           .then(function() {
@@ -29,8 +46,5 @@ module.exports = function(yargs) {
           });
       };
     }
-
   });
-
-
 };
